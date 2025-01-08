e79f160e433940967d517d4733ffa3e6b824211b
Harys <harysrandrianomenjanahary@gmail.com>
2025-01-08 22:16:23 +0100

    contentful access token

--- src/contentfulClient.js
+++ src/contentfulClient.js
@@ -2,7 +2,7 @@ import { createClient } from 'contentful';
 
 const client = createClient({
   space:'wwhr6qah8mau', 
-  accessToken: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN, 
+  accessToken: import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN, 
 });
 
 export default client;
